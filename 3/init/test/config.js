1b1f0ca3a1fc692127005030234cf69a5104733c
Lukas Niebler <nieblerlukas@gmail.com>
2021-11-07 10:11:03 +0100

    extend tests

--- 3/init/test/config.js
+++ 3/init/test/config.js
@@ -3,7 +3,8 @@ module.exports = ({
         baseURL : 'http://localhost:8080/',
         timeout: 50000,
         username: 'admin@gmail.com',
-        password: 'admin'
+        password: 'admin',
+        launchOptions: { headless: false }
     },
     test: {},
     prod: {}
--- 3/init/test/login.tests.js
+++ 3/init/test/login.tests.js
@@ -10,7 +10,7 @@ describe('Login Page', () => {
     let pageModel;
 
     before(async () => {
-        browser = await puppeteer.launch({ headless: false });
+        browser = await puppeteer.launch( config.launchOptions );
     });
 
     beforeEach(async () => {
@@ -29,6 +29,32 @@ describe('Login Page', () => {
 
     it('Should have the right title', async () => {
         (await pageModel.title()).should.equal('Login');
+    });
+
+    it('It should persist the user', async() => {
+      const userDataDir = fs.mkdtempSync('profile');
+      const options = config.launchOptions;
+      options.userDataDir = userDataDir;
+      let persistentBrowser = await puppeteer.launch(options);
+      let persistentPage = await persistentBrowser.newPage();
+      let loginModel = new LoginPageModel(persistentPage, config);
+
+      await loginModel.go();
+      (await loginModel.logState()).should.equal('Login');
+      await loginModel.login(config.username, config.password);
+      (await loginModel.logState()).should.equal('Logout');
+
+      await persistentBrowser.close();
+
+      persistentBrowser= await puppeteer.launch(options);
+      persistentPage = await persistentBrowser.newPage();
+      loginModel = new LoginPageModel(persistentPage, config);
+      await loginModel.go();
+
+      (await loginModel.logState()).should.equal('Logout');
+
+      await persistentBrowser.close();
+      deleteFolderRecursive(userDataDir);
     });
 
     const deleteFolderRecursive = function(path) {
